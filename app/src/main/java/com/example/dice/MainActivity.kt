d1d6e1ea2b0e43dae8ca4ca7c9a39417836c5dc8
alvinjhson <alvinbest26@gmail.com>
2023-11-07 16:09:19 +0100

    You can now choose how many sides the dice have

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.example.dice
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
-import android.widget.TextView
+import android.text.InputType;
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceTextView: TextView
+    lateinit var nameEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //diceTextView = findViewById(R.id.textView) // Hitta din TextView här
-        //val diceValue = randomDiceNumber() // Generera ett tärningsvärde
-        //val diceValueString = diceValue.toString() // Konvertera int till String
-        //diceTextView.text = diceValueString // Sätt strängen som text i TextView
-        //Log.d("!!!", "Dicenumber:$diceValue"  )
+        diceTextView = findViewById(R.id.textView)
+        nameEditText = findViewById(R.id.editTextText)
+        nameEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
         var button = findViewById<Button>(R.id.button1)
         button.setOnClickListener {
-            diceTextView = findViewById(R.id.textView) // Hitta din TextView här
-            val diceValue = randomDiceNumber() // Generera ett tärningsvärde
-            val diceValueString = diceValue.toString() // Konvertera int till String
-            diceTextView.text = diceValueString // Sätt strängen som text i TextView
+            val name = getnameFromEditText()
+            val lastValue = name.toString()
+            setText(lastValue)
         }
     }
     fun randomDiceNumber() : Int {
@@ -29,6 +31,17 @@ class MainActivity : AppCompatActivity() {
         val randomNum = random.nextInt(6) + 1
         return randomNum
     }
+    fun getnameFromEditText() : Int {
+        val name = nameEditText.text.toString()
+        val naem = name.toInt()
+        val random = Random()
+        val randomInput = random.nextInt(naem) + 1
+        return randomInput
+    }
+    fun setText(name :String){
+        diceTextView.text = "Dice Value $name"
+    }
+
 
 
 
